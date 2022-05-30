b308145e4cf93aa173c779d9b2beb27326066ac2
Cristian Amodeo <[cristian.amodeo@gmail.com]>
2022-05-30 12:25:26 -0300

    Desafio1

--- Desafio1/EJ1/js/app.js
+++ Desafio1/EJ1/js/app.js
@@ -1,5 +1,4 @@
 //Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada
-//let ingresarNumero = Number(prompt("ingrese numero"));
 
 let ingresarNumero = Number(prompt("ingrese numero"));
 let acumula='';
